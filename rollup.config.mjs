3e4c91d9fe1ea27071dbc8e7e396f5eaf5b6a300
srhtak <srhtak777@gmail.com>
2024-08-04 15:07:23 +0300

    chore: Update rollup.config.mjs and package.json

---
+++ rollup.config.mjs
@@ -0,0 +1,24 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import typescript from "@rollup/plugin-typescript";
+import dts from "rollup-plugin-dts";
+import postcss from "rollup-plugin-postcss";
+import packageJson from "./package.json" assert { type: "json" };
+
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+  },
+];
